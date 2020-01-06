80e743375fab3bc9dc2588469015badbba13a823
David Latham <latham@davelink.net>
2020-01-06 11:06:07 -0800

    update webhooks example to include all triggers

--- src/examples/java/com/nylas/examples/WebhooksExample.java
+++ src/examples/java/com/nylas/examples/WebhooksExample.java
@@ -22,22 +22,42 @@ public class WebhooksExample {
 			System.out.println(webhook);
 		}
 		
-		Webhook webhook = webhooks.get("5jxt7iuly3oi2h7j6d9yehnr9");
-		System.out.println(webhook);
+		//Webhook webhook = webhooks.get("5jxt7iuly3oi2h7j6d9yehnr9");
+		//System.out.println(webhook);
 		
 		String callbackUrl = props.getProperty("webhook.callback.url");
 		Webhook newWebhook = new Webhook();
 		newWebhook.setCallbackUrl(callbackUrl);
-		newWebhook.setTriggers(Arrays.asList("message.created", "thread.replied"));
+		newWebhook.setTriggers(Arrays.asList(
+				"account.connected",
+				"account.running",
+				"account.stopped",
+				"account.invalid",
+				"account.sync_error",
+				"message.created",
+				"message.opened",
+				"message.link_clicked",
+				"thread.replied",
+				"contact.created",
+				"contact.updated",
+				"contact.deleted",
+				"calendar.created",
+				"calendar.updated",
+				"calendar.deleted",
+				"event.created",
+				"event.updated",
+				"event.deleted"));
+		
+		
 		Webhook created = webhooks.create(newWebhook);
 		System.out.println("Created: " + created);
 		
-		created.setState("inactive");
-		Webhook updated = webhooks.update(created);
-		System.out.println("Updated: " + updated);
-		
-		webhooks.delete(updated.getId());
-		System.out.println("Deleted.");
+//		created.setState("inactive");
+//		Webhook updated = webhooks.update(created);
+//		System.out.println("Updated: " + updated);
+//		
+//		webhooks.delete(updated.getId());
+//		System.out.println("Deleted.");
 	}
 
 }
