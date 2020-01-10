798791377f59ab92c2cbbbf362dd288c5b7c2981
David Latham <latham@davelink.net>
2020-01-10 13:25:20 -0800

    add support for more message filters in DraftQuery

--- src/main/java/com/nylas/DraftQuery.java
+++ src/main/java/com/nylas/DraftQuery.java
@@ -1,35 +1,5 @@
 package com.nylas;
 
-import java.util.Arrays;
+public class DraftQuery extends MessageObjectQuery<DraftQuery> {
 
-import okhttp3.HttpUrl;
-
-public class DraftQuery extends PaginatedQuery<DraftQuery> {
-
-	private String anyEmail;
-
-	@Override
-	public void addParameters(HttpUrl.Builder url) {
-		super.addParameters(url);  // must call through
-		
-		if (anyEmail != null) {
-			url.addQueryParameter("any_email", anyEmail);
-		}
-	}
-	
-	public DraftQuery anyEmail(String... emails) {
-		return anyEmail(Arrays.asList(emails));
-	}
-	
-	public DraftQuery anyEmail(Iterable<String> emails) {
-		if (emails == null) {
-			this.anyEmail = null;
-		} else {
-			this.anyEmail = String.join(",", emails);
-			if (this.anyEmail.isEmpty()) {
-				this.anyEmail = null;
-			}
-		}
-		return this;
-	}
 }
--- src/main/java/com/nylas/MessageQuery.java
+++ src/main/java/com/nylas/MessageQuery.java
@@ -1,65 +1,21 @@
 package com.nylas;
 
 import java.time.Instant;
-import java.util.Arrays;
 
 import okhttp3.HttpUrl;
 
-public class MessageQuery extends PaginatedQuery<MessageQuery> {
+public class MessageQuery extends MessageObjectQuery<MessageQuery> {
 
-	private String subject;
-	private String anyEmail;
-	private String to;
 	private String from;
-	private String cc;
-	private String bcc;
-	private String in;
-	private Boolean unread;
-	private Boolean starred;
-	private String filename;
-	
-	private String threadId;
 	private Long receivedBefore;
 	private Long receivedAfter;
-	private Boolean hasAttachment;
 	
 	@Override
 	public void addParameters(HttpUrl.Builder url) {
 		super.addParameters(url);  // must call through
 		
-		if (subject != null) {
-			url.addQueryParameter("subject", subject);
-		}
-		if (anyEmail != null) {
-			url.addQueryParameter("any_email", anyEmail);
-		}
-		if (to != null) {
-			url.addQueryParameter("to", to);
-		}
 		if (from != null) {
 			url.addQueryParameter("from", from);
-		}
-		if (cc != null) {
-			url.addQueryParameter("cc", cc);
-		}
-		if (bcc != null) {
-			url.addQueryParameter("bcc", bcc);
-		}
-		if (in != null) {
-			url.addQueryParameter("in", in);
-		}
-		if (unread != null) {
-			url.addQueryParameter("unread", unread.toString());
-		}
-		if (starred != null) {
-			url.addQueryParameter("starred", starred.toString());
-		}
-		if (filename != null) {
-			url.addQueryParameter("filename", filename);
-		}
-		
-		if (threadId != null) {
-			url.addQueryParameter("thread_id", threadId);
 		}
 		if (receivedBefore != null) {
 			url.addQueryParameter("received_before", receivedBefore.toString());
@@ -67,74 +23,10 @@ public class MessageQuery extends PaginatedQuery<MessageQuery> {
 		if (receivedAfter != null) {
 			url.addQueryParameter("received_after", receivedAfter.toString());
 		}
-		if (hasAttachment != null) {
-			url.addQueryParameter("has_attachment", hasAttachment.toString());
-		}
-	}
-	
-	public MessageQuery subject(String subject) {
-		this.subject = subject;
-		return this;
-	}
-	
-	public MessageQuery anyEmail(String... emails) {
-		return anyEmail(Arrays.asList(emails));
-	}
-	
-	public MessageQuery anyEmail(Iterable<String> emails) {
-		if (emails == null) {
-			this.anyEmail = null;
-		} else {
-			this.anyEmail = String.join(",", emails);
-			if (this.anyEmail.isEmpty()) {
-				this.anyEmail = null;
-			}
-		}
-		return this;
-	}
-	
-	public MessageQuery to(String to) {
-		this.to = to;
-		return this;
 	}
 	
 	public MessageQuery from(String from) {
 		this.from = from;
-		return this;
-	}
-	
-	public MessageQuery cc(String cc) {
-		this.cc = cc;
-		return this;
-	}
-	
-	public MessageQuery bcc(String bcc) {
-		this.bcc = bcc;
-		return this;
-	}
-	
-	public MessageQuery in(String in) {
-		this.in = in;
-		return this;
-	}
-	
-	public MessageQuery unread(boolean unread) {
-		this.unread = unread;
-		return this;
-	}
-	
-	public MessageQuery starred(boolean starred) {
-		this.starred = starred;
-		return this;
-	}
-	
-	public MessageQuery filename(String filename) {
-		this.filename = filename;
-		return this;
-	}
-	
-	public MessageQuery threadId(String threadId) {
-		this.threadId = threadId;
 		return this;
 	}
 	
@@ -155,10 +47,4 @@ public class MessageQuery extends PaginatedQuery<MessageQuery> {
 	public MessageQuery receivedAfter(Instant receivedAfter) {
 		return receivedAfter(receivedAfter.getEpochSecond());
 	}
-	
-	public MessageQuery hasAttachment(Boolean hasAttachment) {
-		this.hasAttachment = hasAttachment;
-		return this;
-	}
-
 }
