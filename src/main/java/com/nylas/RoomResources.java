69006a9d028eb1f56e741d5834bc91eb0f50ce61
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2021-06-30 17:11:44 -0400

    Convert spaces to tabs

--- src/main/java/com/nylas/RoomResources.java
+++ src/main/java/com/nylas/RoomResources.java
@@ -4,15 +4,15 @@ import java.io.IOException;
 
 public class RoomResources extends RestfulDAO<RoomResource> {
 
-    RoomResources(NylasClient client, String accessToken) {
-        super(client, RoomResource.class, "resources", accessToken);
-    }
+	RoomResources(NylasClient client, String accessToken) {
+		super(client, RoomResource.class, "resources", accessToken);
+	}
 
-    /**
-     * Lists all the room resources associated with the account.
-     * Note that currently this is the only operation that room resources supports
-     */
-    public RemoteCollection<RoomResource> list() throws IOException, RequestFailedException {
-        return list(new RoomResourceQuery());
-    }
+	/**
+	 * Lists all the room resources associated with the account.
+	 * Note that currently this is the only operation that room resources supports
+	 */
+	public RemoteCollection<RoomResource> list() throws IOException, RequestFailedException {
+		return list(new RoomResourceQuery());
+	}
 }
