4e037cbbd972132a59abaaed1a5ba3876e2cb148
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2021-12-02 15:00:15 -0500

    Refactor RestfulModel in preparation for scheduler package

--- src/main/java/com/nylas/RestfulModel.java
+++ src/main/java/com/nylas/RestfulModel.java
@@ -1,11 +1,9 @@
 package com.nylas;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
-public abstract class RestfulModel {
+public abstract class RestfulModel extends Model {
 
 	private String id;
 	
@@ -26,16 +24,6 @@ public abstract class RestfulModel {
 	 */
 	public String getJobStatusId() {
 		return job_status_id;
-	}
-	
-	/**
-	 * Return the set of fields used for model create/update (POST/PUT)
-	 * 
-	 * Default is none, for types that do not allow writes.  Subclasses that
-	 * allow for writes should override and implement
-	 */
-	Map<String, Object> getWritableFields(@SuppressWarnings("unused") boolean creation) {
-		return Collections.emptyMap();
 	}
 	
 	public static List<String> getIds(Iterable<? extends RestfulModel> models) {
