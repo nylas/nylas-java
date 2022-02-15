606a5671f1dbabca9fb6ce9db4e857bd5ea8d9f7
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-02-15 13:07:01 -0500

    Add support for `latestCursor` and `since`

--- src/main/java/com/nylas/Deltas.java
+++ src/main/java/com/nylas/Deltas.java
@@ -15,6 +15,21 @@ public class Deltas {
 		this.accessToken = accessToken;
 	}
 
+	public String latestCursor() throws RequestFailedException, IOException {
+		Map<String, String> response = client.executePost(accessToken, deltaEndpoint().addPathSegment("latest_cursor"), null, Map.class);
+		String cursor = response.get("cursor");
+		if(cursor == null) {
+			throw new RuntimeException("Unexpected response from the API server. Returned 200 but no 'cursor' string found.");
+		}
+
+		return cursor;
+	}
+
+	public DeltaCursor since(String cursor) throws RequestFailedException, IOException {
+		HttpUrl.Builder url = deltaEndpoint().addQueryParameter("cursor", cursor);
+		return client.executeGet(accessToken, url, DeltaCursor.class);
+	}
+
 	private HttpUrl.Builder deltaEndpoint() {
 		return client.newUrlBuilder().addPathSegment("delta");
 	}
