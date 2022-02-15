177fcadb5ebf281010284420c36ee1b106a9dba2
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-02-15 17:50:20 -0500

    Add support for Delta Streaming

--- src/main/java/com/nylas/Deltas.java
+++ src/main/java/com/nylas/Deltas.java
@@ -3,9 +3,18 @@ package com.nylas;
 import com.nylas.delta.Delta;
 import com.nylas.delta.DeltaCursor;
 import com.nylas.delta.DeltaStreamListener;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.JsonEncodingException;
+import com.squareup.moshi.Types;
 import okhttp3.HttpUrl;
+import okhttp3.ResponseBody;
+import okio.Buffer;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Deltas {
@@ -31,6 +40,49 @@ public class Deltas {
 	public DeltaCursor since(String cursor) throws RequestFailedException, IOException {
 		HttpUrl.Builder url = deltaEndpoint().addQueryParameter("cursor", cursor);
 		return client.executeGet(accessToken, url, DeltaCursor.class);
+	}
+
+	public List<Delta<? extends AccountOwnedModel>> stream(String cursor) throws RequestFailedException, IOException {
+		return stream(cursor, null);
+	}
+
+	public List<Delta<? extends AccountOwnedModel>> stream(String cursor, DeltaStreamListener listener)
+			throws RequestFailedException, IOException {
+		List<Delta<? extends AccountOwnedModel>> deltas = new ArrayList<>();
+		Type deltaAdapterType = Types.newParameterizedType(Delta.class, AccountOwnedModel.class);
+		JsonAdapter<Delta<? extends AccountOwnedModel>> adapter = JsonHelper.moshi().adapter(deltaAdapterType);
+		HttpUrl.Builder url = deltaEndpoint()
+				.addPathSegment("streaming")
+				.addQueryParameter("cursor", cursor);
+
+		ResponseBody responseBody = client.download(accessToken, url);
+		StringBuilder jsonBuilder = new StringBuilder();
+		try {
+			while(!responseBody.source().exhausted()) {
+				Buffer buffer = new Buffer();
+				long count = responseBody.source().read(buffer, 8192);
+				if(count > 1) {
+					String[] jsonStream = buffer.readUtf8().split("\n");
+					for(String json : jsonStream) {
+						jsonBuilder.append(json);
+						try {
+							Delta<? extends AccountOwnedModel> delta = adapter.fromJson(jsonBuilder.toString());
+							deltas.add(delta);
+							if(listener != null) {
+								listener.onDelta(delta);
+							}
+							jsonBuilder.setLength(0);
+						} catch (JsonEncodingException | EOFException e) {
+							// The JSON was partial, move on to the next string/chunk
+						}
+					}
+				}
+			}
+		} catch (IllegalStateException e) {
+			// Stream was closed, return the deltas we have
+		}
+
+		return deltas;
 	}
 
 	private HttpUrl.Builder deltaEndpoint() {
