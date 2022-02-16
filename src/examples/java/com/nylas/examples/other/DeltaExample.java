0172c2e9fc2b33f564a60764cea06205b326393c
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-02-16 16:47:39 -0500

    Add example for Delta

---
+++ src/examples/java/com/nylas/examples/other/DeltaExample.java
@@ -0,0 +1,54 @@
+package com.nylas.examples.other;
+
+import com.nylas.*;
+import com.nylas.delta.*;
+import com.nylas.examples.ExampleConf;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+class DeltaProcessor implements DeltaStreamListener, DeltaLongPollListener {
+
+	@Override
+	public void onDelta(Delta<? extends AccountOwnedModel> delta) {
+		System.out.println("Delta received! " + delta);
+	}
+
+	@Override
+	public void onDeltaCursor(DeltaCursor deltaCursor) {
+		System.out.println("Delta Cursor received! " + deltaCursor);
+	}
+}
+
+public class DeltaExample {
+
+	private static final Logger log = LogManager.getLogger(DeltaExample.class);
+
+	public static void main(String[] args) throws RequestFailedException, IOException {
+		ExampleConf conf = new ExampleConf();
+		NylasClient client = new NylasClient();
+		NylasAccount account = client.account(conf.get("access.token"));
+		Deltas deltas = account.deltas();
+
+		// Get the latest cursor
+		String latestCursor = deltas.latestCursor();
+		log.info("The latest cursor is: " + latestCursor);
+
+		// Get all the delta cursors since the latest cursor
+		DeltaCursor deltasSinceLatestCursor = deltas.since(latestCursor);
+		log.info("The delta cursors since the latest cursor: " + deltasSinceLatestCursor);
+
+		// Create an instance of our class that will listen for new Delta and DeltaCursor types
+		DeltaProcessor deltaProcessor = new DeltaProcessor();
+
+		// Stream deltas since the latest cursor
+		deltas.stream(latestCursor);
+
+		// Long-poll for deltas, with options passed in
+		DeltaQueryOptions options = new DeltaQueryOptions()
+				.setView("expanded")
+				.includeTypes(DeltaQueryOptions.Type.EVENT, DeltaQueryOptions.Type.MESSAGE);
+		deltas.longpoll(latestCursor, 30, deltaProcessor, options);
+	}
+}
