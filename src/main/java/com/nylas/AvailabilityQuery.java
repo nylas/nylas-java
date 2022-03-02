076b406bfb85e96d07a4a8ac2837ea75b2a6bec5
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-03-02 16:30:18 -0500

    Add missing import

--- src/main/java/com/nylas/AvailabilityQuery.java
+++ src/main/java/com/nylas/AvailabilityQuery.java
@@ -1,10 +1,7 @@
 package com.nylas;
 
 import java.time.Instant;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 abstract class AvailabilityQuery <Q extends AvailabilityQuery <Q>> {
 
