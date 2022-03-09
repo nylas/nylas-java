45c5b268e728d6553563bd32aab4fb680d9dfe2a
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-03-09 18:25:22 -0500

    Add an example for Integrations

---
+++ src/examples/java/com/nylas/examples/other/IntegrationsExample.java
@@ -0,0 +1,41 @@
+package com.nylas.examples.other;
+
+import com.nylas.*;
+import com.nylas.examples.ExampleConf;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class IntegrationsExample {
+
+	private static final Logger log = LogManager.getLogger(IntegrationsExample.class);
+
+	public static void main(String[] args) throws Exception {
+		ExampleConf conf = new ExampleConf();
+		NylasClient client = new NylasClient();
+		NylasApplication application = client.application(conf.get("nylas.client.id"), conf.get("nylas.client.secret"));
+		Integrations integrations = application.integrations();
+
+		log.info("Listing all integrations:");
+		RemoteCollection<Integration> allIntegrations = integrations.list();
+		for(Integration integration : allIntegrations) {
+			log.info("Integration: " + integration);
+		}
+
+		log.info("Creating a new Zoom integration");
+		Integration integration = new Integration("Test Zoom Integration");
+		integration.setClientId(conf.get("zoom.client.id"));
+		integration.setClientSecret(conf.get("zoom.client.secret"));
+		integration.addRedirectUris("https://www.nylas.com");
+		integration.setExpiresIn(1209600L);
+		Integration created = integrations.create(integration, Integration.Provider.ZOOM);
+		log.info("Created: " + created);
+
+		log.info("Updating the integration");
+		created.setName("Updated Test Zoom Integration");
+		Integration update = integrations.update(created);
+		log.info("Updated: " + update);
+
+		log.info("Deleting integration");
+		integrations.delete(Integration.Provider.ZOOM);
+	}
+}
