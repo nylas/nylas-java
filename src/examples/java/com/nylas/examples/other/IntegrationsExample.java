78398453f6fcd3c8db365770f9e961195690ad15
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-03-24 18:28:27 -0400

    Update Integrations example with Grants

--- src/examples/java/com/nylas/examples/other/IntegrationsExample.java
+++ src/examples/java/com/nylas/examples/other/IntegrationsExample.java
@@ -5,6 +5,10 @@ import com.nylas.examples.ExampleConf;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
 public class IntegrationsExample {
 
 	private static final Logger log = LogManager.getLogger(IntegrationsExample.class);
@@ -14,12 +18,7 @@ public class IntegrationsExample {
 		NylasClient client = new NylasClient();
 		NylasApplication application = client.application(conf.get("nylas.client.id"), conf.get("nylas.client.secret"));
 		Integrations integrations = application.integrations();
-
-		log.info("Listing all integrations:");
-		RemoteCollection<Integration> allIntegrations = integrations.list();
-		for(Integration integration : allIntegrations) {
-			log.info("Integration: " + integration);
-		}
+		Grants grants = application.grants();
 
 		log.info("Creating a new Zoom integration");
 		Integration integration = new Integration("Test Zoom Integration");
@@ -30,6 +29,12 @@ public class IntegrationsExample {
 		Integration created = integrations.create(integration, Integration.Provider.ZOOM);
 		log.info("Created: " + created);
 
+		log.info("Listing all integrations:");
+		RemoteCollection<Integration> allIntegrations = integrations.list();
+		for(Integration currentIntegration : allIntegrations) {
+			log.info("Integration: " + currentIntegration);
+		}
+
 		log.info("Updating the integration");
 		created.setName("Updated Test Zoom Integration");
 		created.setClientId(conf.get("zoom.client.id"));
@@ -37,7 +42,26 @@ public class IntegrationsExample {
 		Integration update = integrations.update(created);
 		log.info("Updated: " + update);
 
+		grantsExample(conf, grants);
+
 		log.info("Deleting integration");
 		integrations.delete(Integration.Provider.ZOOM);
+	}
+
+	private static void grantsExample(ExampleConf conf, Grants grants) throws RequestFailedException, IOException {
+		log.info("Creating a new Zoom grant");
+		Map<String, String> settings = Collections.singletonMap("refresh_token", conf.get("zoom.client.refresh_token"));
+		Grant grant = new Grant(Integration.Provider.ZOOM, settings);
+		Grant created = grants.create(grant);
+		log.info("Created: " + created);
+
+		log.info("Listing all grants:");
+		RemoteCollection<Grant> allGrants = grants.list();
+		for(Grant currentGrant : allGrants) {
+			log.info("Grant: " + currentGrant);
+		}
+
+		log.info("Deleting grant");
+		grants.delete(created.getId());
 	}
 }
