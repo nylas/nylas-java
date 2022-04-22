e2cc461cf9c4a7d95e9ea5af739d1bf5c5703eee
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-04-22 13:53:12 -0400

    Add missing `order` field in `Availability`

--- src/main/java/com/nylas/Availability.java
+++ src/main/java/com/nylas/Availability.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 public class Availability {
 	private List<TimeSlot> time_slots;
+	private List<String> order;
 
 	public List<TimeSlot> getTimeSlots() {
 		return time_slots;
 	}
+	public List<String> getOrder() {
+		return order;
+	}
 
 	@Override
 	public String toString() {
-		return "Availability [time_slots=" + time_slots + "]";
+		return "Availability [time_slots=" + time_slots + ", order=" + order + "]";
 	}
 }
