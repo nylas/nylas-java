392094b20c83c503efb9d6b1a681425827e53ae7
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-11-29 20:19:42 -0500

    Add enum for Triggers and State
    Also add support for them in setters in the Webhook object

--- src/main/java/com/nylas/Webhook.java
+++ src/main/java/com/nylas/Webhook.java
@@ -3,6 +3,8 @@ package com.nylas;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Webhook extends RestfulModel {
 
@@ -11,6 +13,49 @@ public class Webhook extends RestfulModel {
 	private String state;
 	private List<String> triggers;
 	private String version;
+
+	/**
+	 * Enumeration containing the different webhook triggers
+	 */
+	public enum Trigger {
+		AccountConnected("account.connected"),
+		AccountRunning("account.running"),
+		AccountStopped("account.stopped"),
+		AccountInvalid("account.invalid"),
+		AccountSyncError("account.sync_error"),
+		MessageBounced("message.bounced"),
+		MessageCreated("message.created"),
+		MessageOpened("message.opened"),
+		MessageUpdated("message.updated"),
+		MessageLinkClicked("message.link_clicked"),
+		ThreadReplied("thread.replied"),
+		ContactCreated("contact.created"),
+		ContactUpdated("contact.updated"),
+		ContactDeleted("contact.deleted"),
+		CalendarCreated("calendar.created"),
+		CalendarUpdated("calendar.updated"),
+		CalendarDeleted("calendar.deleted"),
+		EventCreated("event.created"),
+		EventUpdated("event.updated"),
+		EventDeleted("event.deleted"),
+		JobSuccessful("job.successful"),
+		JobFailed("job.failed");
+
+		private final String name;
+
+		Trigger(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+
+	public enum State {
+		ACTIVE,
+		INACTIVE;
+	}
 	
 	public String getApplicationId() {
 		return application_id;
@@ -40,8 +85,18 @@ public class Webhook extends RestfulModel {
 		this.state = state;
 	}
 
+	public void setState(State state) {
+		this.state = state.toString().toLowerCase();
+	}
+
 	public void setTriggers(List<String> triggers) {
 		this.triggers = triggers;
+	}
+
+	public void setTriggers(Trigger... triggers) {
+		this.triggers = Stream.of(triggers)
+				.map(Trigger::name)
+				.collect(Collectors.toList());
 	}
 
 	@Override
