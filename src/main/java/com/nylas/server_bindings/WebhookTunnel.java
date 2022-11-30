a2943bd3446f2a46f8abd83996b9c33d4a4c274f
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-11-29 20:41:44 -0500

    Add documentation to WebhookTunnel

--- src/main/java/com/nylas/server_bindings/WebhookTunnel.java
+++ src/main/java/com/nylas/server_bindings/WebhookTunnel.java
@@ -13,6 +13,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Sets up and registers a websocket connection for local webhook testing
+ */
 public class WebhookTunnel extends WebSocketClient {
 
 	private final NylasApplication app;
@@ -34,6 +37,10 @@ public class WebhookTunnel extends WebSocketClient {
 		this.setHeaders();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * Also registers the webhook with the Nylas API.
+	 */
 	@Override
 	public void connect() {
 		super.connect();
@@ -45,12 +52,20 @@ public class WebhookTunnel extends WebSocketClient {
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * Calls {@link WebhookHandler#onOpen(short)}
+	 */
 	@Override
 	public void onOpen(ServerHandshake handshakedata) {
 		log.trace("Opening websocket connection");
 		webhookHandler.onOpen(handshakedata.getHttpStatus());
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * Calls {@link WebhookHandler#onMessage(Notification)}
+	 */
 	@Override
 	public void onMessage(String message) {
 		log.trace("Messaged received from websocket");
@@ -63,18 +78,29 @@ public class WebhookTunnel extends WebSocketClient {
 		log.trace("Not a valid delta response, skipping.");
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * Calls {@link WebhookHandler#onClose(int, String, boolean)}
+	 */
 	@Override
 	public void onClose(int code, String reason, boolean remote) {
 		log.trace("Closing websocket connection");
 		webhookHandler.onClose(code, reason, remote);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * Calls {@link WebhookHandler#onError(Exception)}
+	 */
 	@Override
 	public void onError(Exception ex) {
 		log.trace("Error encountered during websocket connection");
 		webhookHandler.onError(ex);
 	}
 
+	/**
+	 * Sets the headers necessary for setting up the websocket tunnel
+ 	 */
 	private void setHeaders() {
 		this.addHeader("Client-Id", app.getClientId());
 		this.addHeader("Client-Secret", app.getClientSecret());
@@ -82,6 +108,12 @@ public class WebhookTunnel extends WebSocketClient {
 		this.addHeader("Region", region);
 	}
 
+	/**
+	 * Registers the websocket connection and the callback with your Nylas application
+	 * @param app The configured Nylas application
+	 * @param tunnelId The UUID generated for the websocket tunnel
+	 * @param triggers The triggers to subscribe to
+	 */
 	private void registerWebhookCallback(NylasApplication app, String tunnelId, List<String> triggers)
 			throws RequestFailedException, IOException {
 		Webhook webhook = new Webhook();
@@ -91,38 +123,63 @@ public class WebhookTunnel extends WebSocketClient {
 		app.webhooks().create(webhook);
 	}
 
+	/**
+	 * A builder for {@link WebhookTunnel}
+	 */
 	public static class Builder {
 		private final NylasApplication app;
 		private final WebhookHandler webhookHandler;
 		private String region = "us";
-		private List<String> triggers = convertScopesToString(Webhook.Trigger.values());
+		private List<String> triggers = convertTriggersToString(Webhook.Trigger.values());
 
 		public Builder(NylasApplication app, WebhookHandler webhookHandler) {
 			this.app = app;
 			this.webhookHandler = webhookHandler;
 		}
 
+		/**
+		 * Set the region to configure the websocket to
+		 * @param region The Nylas region
+		 * @return The builder with the region set
+		 */
 		public Builder region(String region) {
 			this.region = region;
 			return this;
 		}
 
+		/**
+		 * Set the webhook trigger(s) to subscribe to
+		 * @param triggers The webhook trigger(s) to subscribe to
+		 * @return The builder with the trigger(s) set
+		 */
 		public Builder triggers(Webhook.Trigger... triggers) {
-			this.triggers = convertScopesToString(triggers);
+			this.triggers = convertTriggersToString(triggers);
 			return this;
 		}
 
+		/**
+		 * Builds the WebhookTunnel
+		 * @return The configured WebhookTunnel
+		 */
 		public WebhookTunnel build() throws URISyntaxException {
 			return new WebhookTunnel(this);
 		}
 
-		private static List<String> convertScopesToString(Webhook.Trigger[] triggers) {
+		/**
+		 * Helper method that converts a list of triggers to their string values
+		 * @param triggers The list of triggers to convert
+		 * @return A list of strings with the value of the provided triggers
+		 */
+		private static List<String> convertTriggersToString(Webhook.Trigger[] triggers) {
 			return Stream.of(triggers)
 					.map(Webhook.Trigger::getName)
 					.collect(Collectors.toList());
 		}
 	}
 
+	/**
+	 * An interface for implementing classes to handle events from the {@link WebhookTunnel}
+	 */
 	public interface WebhookHandler {
 		void onOpen(short httpStatusCode);
 		void onClose(int code, String reason, boolean remote);
