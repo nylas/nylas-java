d4f6ea30ed7af0685344474efc69cf958f8a2d6a
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-02-10 16:37:12 -0500

    Add missing types to polymorphic json factory

--- src/main/java/com/nylas/delta/Delta.java
+++ src/main/java/com/nylas/delta/Delta.java
@@ -41,6 +41,9 @@ public class Delta<T extends AccountOwnedModel> extends RestfulModel {
 	// Adapter for identifying the class of object in the delta to serialize to
 	public static final JsonAdapter.Factory ACCOUNT_OWNED_MODEL_JSON_FACTORY
 			= PolymorphicJsonAdapterFactory.of(AccountOwnedModel.class, "object")
+			.withSubtype(Account.class, "account")
+			.withSubtype(Calendar.class, "calendar")
+			.withSubtype(RoomResource.class, "room_resource")
 			.withSubtype(Contact.class, "contact")
 			.withSubtype(File.class, "file")
 			.withSubtype(Message.class, "message")
