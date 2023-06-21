8080cf1f38510c25b8bcb9e32d413cf3354e4cb3
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-06-21 11:09:40 -0400

    Add klint and spotless for lint+formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,68 +1,60 @@
 // TODO::Add back the tasks we took out
 
 plugins {
-    kotlin("jvm") version "1.8.21"
-    application
+  kotlin("jvm") version "1.8.21"
+  id("com.diffplug.spotless") version "6.13.0"
+  application
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 kotlin {
-    jvmToolchain(8)
+  jvmToolchain(8)
 }
 
 dependencies {
-    //TODO::Better clean up dependencies
-    //////////////////////////////////
-    // Public dependencies
+  // TODO::Better clean up dependencies
+  // ////////////////////////////////
+  // Public dependencies
 
-    // OkHttp 3 - Http client (without Kotlin dependency of version 4)
-    api("com.squareup.okhttp3:okhttp:3.14.5")
+  // OkHttp 3 - Http client (without Kotlin dependency of version 4)
+  api("com.squareup.okhttp3:okhttp:3.14.5")
 
-    // Moshi JSON library
-    implementation("com.squareup.moshi:moshi:1.15.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
-    implementation("com.squareup.moshi:moshi-adapters:1.15.0")
+  // Moshi JSON library
+  implementation("com.squareup.moshi:moshi:1.15.0")
+  implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+  implementation("com.squareup.moshi:moshi-adapters:1.15.0")
 
-    // SLF4J for logging facade
-    implementation("org.slf4j:slf4j-api:2.0.7")
-
-    ///////////////////////////////////
-    // Test dependencies
-
-    // jUnit 5 for Testing
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
-
-    // use log4j logging for tests
-    testRuntimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
-
-    // use wiremock for mocking the nylas server during tests
-    testImplementation("com.github.tomakehurst:wiremock-jre8:2.26.3")
-
-    // mockito for dependency mocking
-    dependencies {
-        testImplementation("org.mockito:mockito-core:3.+")
-        testImplementation("org.mockito:mockito-inline:2.13.0")
-    }
+  // SLF4J for logging facade
+  implementation("org.slf4j:slf4j-api:2.0.7")
 }
 
 tasks.register<Jar>("uberJar") {
-    archiveClassifier.set("uber")
+  archiveClassifier.set("uber")
 
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
-    from(sourceSets.main.get().output)
+  from(sourceSets.main.get().output)
 
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-    })
+  dependsOn(configurations.runtimeClasspath)
+  from({
+    configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+  })
+}
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+  kotlin {
+    ktlint()
+  }
+  kotlinGradle {
+    target("*.gradle.kts") // default target for kotlinGradle
+    ktlint() // or ktfmt() or prettier()
+  }
 }
