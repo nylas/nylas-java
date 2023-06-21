03a2e4318ab015bb41978d2c2f3d151a27c70a0b
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-06-21 19:52:24 -0400

    clean up gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,47 @@
-// TODO::Add back the tasks we took out
-
 plugins {
-    kotlin("jvm") version "1.8.21"
-    id("org.jmailen.kotlinter") version "3.15.0"
-    application
+  kotlin("jvm") version "1.8.21"
+  id("org.jmailen.kotlinter") version "3.15.0"
+  application
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 kotlin {
-    jvmToolchain(8)
+  jvmToolchain(8)
 }
 
 dependencies {
-    // TODO::Better clean up dependencies
-    // ////////////////////////////////
-    // Public dependencies
+  // ////////////////////////////////
+  // Public dependencies
 
-    // OkHttp 3 - Http client (without Kotlin dependency of version 4)
-    api("com.squareup.okhttp3:okhttp:3.14.5")
+  // OkHttp 3 - Http client (without Kotlin dependency of version 4)
+  api("com.squareup.okhttp3:okhttp:3.14.5")
 
-    // Moshi JSON library
-    implementation("com.squareup.moshi:moshi:1.15.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
-    implementation("com.squareup.moshi:moshi-adapters:1.15.0")
+  // Moshi JSON library
+  implementation("com.squareup.moshi:moshi:1.15.0")
+  implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+  implementation("com.squareup.moshi:moshi-adapters:1.15.0")
 
-    // SLF4J for logging facade
-    implementation("org.slf4j:slf4j-api:2.0.7")
+  // SLF4J for logging facade
+  implementation("org.slf4j:slf4j-api:2.0.7")
 }
 
 tasks.register<Jar>("uberJar") {
-    archiveClassifier.set("uber")
+  archiveClassifier.set("uber")
 
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
-    from(sourceSets.main.get().output)
+  from(sourceSets.main.get().output)
 
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-    })
+  dependsOn(configurations.runtimeClasspath)
+  from({
+    configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+  })
 }
