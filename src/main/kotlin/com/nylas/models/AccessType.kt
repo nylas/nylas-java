ea3095d5c16596ec1f8759b39a523f69ceeed5b1
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-06-20 21:48:26 -0400

    Helper enums

---
+++ src/main/kotlin/com/nylas/models/AccessType.kt
@@ -0,0 +1,10 @@
+package com.nylas.models
+
+import com.squareup.moshi.Json
+
+enum class AccessType {
+  @Json(name = "offline")
+  OFFLINE,
+  @Json(name = "online")
+  ONLINE,
+}
---
+++ src/main/kotlin/com/nylas/models/AuthProvider.kt
@@ -0,0 +1,14 @@
+package com.nylas.models
+
+import com.squareup.moshi.Json
+
+enum class AuthProvider(val value: String) {
+  @Json(name = "google")
+  GOOGLE("google"),
+  @Json(name = "yahoo")
+  YAHOO("yahoo"),
+  @Json(name = "microsoft")
+  MICROSOFT("microsoft"),
+  @Json(name = "imap")
+  IMAP("imap"),
+}
