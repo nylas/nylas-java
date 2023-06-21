af37ad8007b274c9f6c038229aa7926910b8f57c
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-06-20 21:46:56 -0400

    AV-1659 AV-1660 AV-1661

---
+++ src/main/kotlin/com/nylas/models/UrlForAuthenticationConfig.kt
@@ -0,0 +1,57 @@
+package com.nylas.models
+
+import com.squareup.moshi.Json
+
+data class UrlForAuthenticationConfig(
+  @Json(name = "provider")
+  val provider: AuthProvider,
+  @Json(name = "redirect_uri")
+  val redirectUri: String,
+  @Json(name = "access_type")
+  val accessType: AccessType = AccessType.OFFLINE,
+  @Json(name = "prompt")
+  val prompt: String?,
+  @Json(name = "scope")
+  val scope: List<String>?,
+  @Json(name = "include_grant_scopes")
+  val includeGrantScopes: Boolean?,
+  @Json(name = "metadata")
+  val metadata: String?,
+  @Json(name = "state")
+  val state: String?,
+  @Json(name = "login_hint")
+  val loginHint: String?
+) {
+  data class Builder(
+    private val provider: AuthProvider,
+    private val redirectUri: String
+  ) {
+    private var accessType: AccessType = AccessType.OFFLINE
+    private var prompt: String? = null
+    private var scope: List<String>? = null
+    private var includeGrantScopes: Boolean? = null
+    private var metadata: String? = null
+    private var state: String? = null
+    private var loginHint: String? = null
+
+    fun accessType(accessType: AccessType) = apply { this.accessType = accessType }
+    fun prompt(prompt: String) = apply { this.prompt = prompt }
+    fun scope(scope: List<String>) = apply { this.scope = scope }
+    fun includeGrantScopes(includeGrantScopes: Boolean) = apply { this.includeGrantScopes = includeGrantScopes }
+    fun metadata(metadata: String) = apply { this.metadata = metadata }
+    fun state(state: String) = apply { this.state = state }
+    fun loginHint(loginHint: String) = apply { this.loginHint = loginHint }
+
+    fun build() = UrlForAuthenticationConfig(
+      provider,
+      redirectUri,
+      accessType,
+      prompt,
+      scope,
+      includeGrantScopes,
+      metadata,
+      state,
+      loginHint
+    )
+  }
+}
--- src/main/kotlin/com/nylas/resources/Auth.kt
+++ src/main/kotlin/com/nylas/resources/Auth.kt
@@ -3,7 +3,10 @@ package com.nylas.resources
 import com.nylas.NylasClient
 import com.nylas.models.*
 import com.nylas.util.JsonHelper
+import okhttp3.HttpUrl
 import java.io.IOException
+import java.security.MessageDigest
+import java.util.*
 
 class Auth(
   private val client: NylasClient,
@@ -44,5 +47,48 @@ class Auth(
       .toJson(request)
 
     return client.executePost(path, CodeExchangeResponse::class.java, serializedRequestBody)
+  }
+  @Throws(IOException::class, NylasApiError::class)
+  fun urlForAuthentication(config: UrlForAuthenticationConfig): String {
+    return urlAuthBuilder(config).build().toString()
+  }
+
+  @Throws(IOException::class, NylasApiError::class)
+  fun urlForAuthenticationPKCE(config: UrlForAuthenticationConfig): PKCEAuthURL {
+    val urlBuilder = urlAuthBuilder(config)
+    val secret = UUID.randomUUID().toString();
+
+    val sha256Digest = MessageDigest.getInstance("SHA-256").digest(secret.toByteArray())
+    val secretHash = Base64.getEncoder().encodeToString(sha256Digest)
+
+    urlBuilder
+      .addQueryParameter("code_challenge_method", "s256")
+      .addQueryParameter("code_challenge", secretHash)
+
+    return PKCEAuthURL(urlBuilder.build().toString(), secret, secretHash)
+  }
+
+  @Throws(IOException::class, NylasApiError::class)
+  fun urlForAdminConsent(config: UrlForAuthenticationConfig, credentialId: String): String {
+    val urlBuilder = urlAuthBuilder(config)
+
+    urlBuilder
+      .addQueryParameter("response_type", "adminconsent")
+      .addQueryParameter("credential_id", credentialId)
+
+    return urlBuilder.build().toString()
+  }
+
+  @Throws(IOException::class, NylasApiError::class)
+  private fun urlAuthBuilder(config: UrlForAuthenticationConfig): HttpUrl.Builder {
+    val url = this.client.newUrlBuilder().addPathSegments("/v3/connect/auth")
+    val json = JsonHelper.moshi()
+      .adapter(UrlForAuthenticationConfig::class.java)
+      .toJson(config)
+    JsonHelper.jsonToMap(json).forEach { (key, value) ->
+      url.addQueryParameter(key, value.toString())
+    }
+
+    return url
   }
 }
