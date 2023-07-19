0397d35671c2b644f73c395170f842ea02c0f4b2
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-07-19 11:16:55 -0400

    better ordering for destructuring

--- src/main/kotlin/com/nylas/models/ListResponse.kt
+++ src/main/kotlin/com/nylas/models/ListResponse.kt
@@ -3,13 +3,10 @@ package com.nylas.models
 import com.squareup.moshi.Json
 
 data class ListResponse<T>(
-  @Json(name = "request_id")
-  val requestId: String = "",
   @Json(name = "data")
   val data: List<T> = emptyList(),
+  @Json(name = "request_id")
+  val requestId: String = "",
   @Json(name = "next_cursor")
   val nextCursor: String? = null,
-  // TODO::Define error
-  @Json(name = "error")
-  val error: Any? = null,
 )
--- src/main/kotlin/com/nylas/models/Response.kt
+++ src/main/kotlin/com/nylas/models/Response.kt
@@ -3,11 +3,8 @@ package com.nylas.models
 import com.squareup.moshi.Json
 
 data class Response<T>(
-  @Json(name = "request_id")
-  val requestId: String = "",
   @Json(name = "data")
   val data: T,
-  // TODO::Define error
-  @Json(name = "error")
-  val error: Any? = null,
+  @Json(name = "request_id")
+  val requestId: String = "",
 )
