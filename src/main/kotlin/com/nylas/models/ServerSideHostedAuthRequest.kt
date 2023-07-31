1aef63101574b93f780f2af92733c772e814fb14
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-07-31 14:05:16 -0400

    make provider optional

--- src/main/kotlin/com/nylas/models/ServerSideHostedAuthRequest.kt
+++ src/main/kotlin/com/nylas/models/ServerSideHostedAuthRequest.kt
@@ -3,10 +3,10 @@ package com.nylas.models
 import com.squareup.moshi.Json
 
 data class ServerSideHostedAuthRequest(
-  @Json(name = "provider")
-  val provider: AuthProvider,
   @Json(name = "redirect_uri")
   val redirectUri: String,
+  @Json(name = "provider")
+  val provider: AuthProvider?,
   @Json(name = "scope")
   val scope: List<String>?,
   @Json(name = "state")
@@ -17,22 +17,23 @@ data class ServerSideHostedAuthRequest(
   val cookieNonce: String?,
 ) {
   data class Builder(
-    private val provider: AuthProvider,
     private val redirectUri: String,
   ) {
+    private var provider: AuthProvider? = null
     private var scope: List<String>? = null
     private var state: String? = null
     private var loginHint: String? = null
     private var cookieNonce: String? = null
 
+    fun provider(provider: AuthProvider) = apply { this.provider = provider }
     fun scope(scope: List<String>) = apply { this.scope = scope }
     fun state(state: String) = apply { this.state = state }
     fun loginHint(loginHint: String) = apply { this.loginHint = loginHint }
     fun cookieNonce(cookieNonce: String) = apply { this.cookieNonce = cookieNonce }
 
     fun build() = ServerSideHostedAuthRequest(
-      provider,
       redirectUri,
+      provider,
       scope,
       state,
       loginHint,
