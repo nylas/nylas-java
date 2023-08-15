e3465cb70f1154bd80643c27700c54d56e4a52d7
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-08-14 22:13:19 -0400

    fix: token request should require all fields

--- src/main/kotlin/com/nylas/models/TokenExchangeRequest.kt
+++ src/main/kotlin/com/nylas/models/TokenExchangeRequest.kt
@@ -20,49 +20,16 @@ data class TokenExchangeRequest(
    * Client ID of the application.
    */
   @Json(name = "client_id")
-  var clientId: String? = null,
+  var clientId: String,
   /**
    * Client secret of the application.
    */
   @Json(name = "client_secret")
-  var clientSecret: String? = null,
+  var clientSecret: String,
 ) {
   /**
    * The grant type for the request. For refreshing tokens, it should always be 'refresh_token'.
    */
   @Json(name = "grant_type")
   private var grantType: String = "refresh_token"
-
-  /**
-   * A builder for creating a [TokenExchangeRequest].
-   * @param redirectUri Should match the same redirect URI that was used for getting the code during the initial authorization request.
-   * @param refreshToken Token to refresh/request your short-lived access token
-   */
-  data class Builder(
-    private val redirectUri: String,
-    private val refreshToken: String,
-  ) {
-    private var clientId: String? = null
-    private var clientSecret: String? = null
-
-    /**
-     * Set the client ID.
-     * @param clientId The client ID.
-     * @return The builder.
-     */
-    fun clientId(clientId: String) = apply { this.clientId = clientId }
-
-    /**
-     * Set the client secret.
-     * @param clientSecret The client secret.
-     * @return The builder.
-     */
-    fun clientSecret(clientSecret: String) = apply { this.clientSecret = clientSecret }
-
-    /**
-     * Build the [TokenExchangeRequest].
-     * @return The [TokenExchangeRequest].
-     */
-    fun build() = TokenExchangeRequest(redirectUri, refreshToken, clientId, clientSecret)
-  }
 }
