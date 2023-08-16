c89548239649e1261158cae8c0b17dfba1b6f22c
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-08-16 19:19:42 -0400

    add publish + signing gradle steps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,10 @@ plugins {
   kotlin("jvm") version "1.8.21"
   id("org.jmailen.kotlinter") version "3.15.0"
   id("org.jetbrains.dokka") version "1.8.20"
+  `maven-publish`
+  `java-library`
   application
+  signing
 }
 
 repositories {
@@ -12,6 +15,8 @@ repositories {
 java {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
+  withJavadocJar()
+  withSourcesJar()
 }
 
 kotlin {
@@ -57,4 +62,47 @@ tasks.register<Jar>("uberJar") {
   from({
     configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
   })
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("mavenJava") {
+      from(components["java"])
+      pom {
+        name.set("Nylas SDK for Kotlin & Java")
+        description.set("Kotlin & Java SDK for the Nylas Communication Platform")
+        url.set("https://github.com/nylas/nylas-java")
+        licenses {
+          license {
+            name.set("MIT")
+            url.set("http://www.opensource.org/licenses/mit-license.php")
+          }
+        }
+        developers {
+          developer {
+            id.set("mrashed-dev")
+            name.set("Mostafa Rashed")
+            organization.set("Nylas")
+            organizationUrl.set("https://www.nylas.com/")
+          }
+        }
+        scm {
+          url.set("https://github.com/nylas/nylas-java")
+          connection.set("scm:git:https://github.com/nylas/nylas-java.git")
+          developerConnection.set("scm:git:https://github.com/nylas/nylas-java.git")
+        }
+      }
+    }
+  }
+  repositories {
+    maven {
+      name = "ossrh"
+      url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+      credentials(PasswordCredentials::class)
+    }
+  }
+}
+
+signing {
+  sign(publishing.publications["mavenJava"])
 }
