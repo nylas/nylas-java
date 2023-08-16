9cafffe0c09420578479021bfa1237b7e6e961cb
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-08-16 11:09:47 -0400

    Remove unsupported auth provider

--- src/main/kotlin/com/nylas/models/AuthProvider.kt
+++ src/main/kotlin/com/nylas/models/AuthProvider.kt
@@ -9,9 +9,6 @@ enum class AuthProvider(val value: String) {
   @Json(name = "google")
   GOOGLE("google"),
 
-  @Json(name = "yahoo")
-  YAHOO("yahoo"),
-
   @Json(name = "microsoft")
   MICROSOFT("microsoft"),
 
