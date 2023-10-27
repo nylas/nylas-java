e43b5f830a9128ffb57acc1ca1d8d0fc054cf9af
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-10-27 11:19:40 -0400

    Update documentation

--- src/main/kotlin/com/nylas/models/ListConnectorsQueryParams.kt
+++ src/main/kotlin/com/nylas/models/ListConnectorsQueryParams.kt
@@ -26,17 +26,22 @@ data class ListConnectorsQueryParams(
     /**
      * Set the maximum number of objects to return.
      * This field defaults to 50. The maximum allowed value is 200.
+     * @param limit The maximum number of objects to return.
+     * @return The builder.
      */
     fun limit(limit: Int?) = apply { this.limit = limit }
 
     /**
      * Set the identifier that specifies which page of data to return.
      * This value should be taken from the [ListResponse.nextCursor] response field.
+     * @param pageToken The identifier that specifies which page of data to return.
+     * @return The builder.
      */
     fun pageToken(pageToken: String?) = apply { this.pageToken = pageToken }
 
     /**
      * Build the [ListConnectorsQueryParams] object.
+     * @return The [ListConnectorsQueryParams] object.
      */
     fun build() = ListConnectorsQueryParams(limit, pageToken)
   }
