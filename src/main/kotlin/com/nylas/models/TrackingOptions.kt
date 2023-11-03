e07a43384d16c803512af1eeb46002a68806b6af
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-11-03 17:57:03 -0400

    Fix tracking options

--- src/main/kotlin/com/nylas/models/TrackingOptions.kt
+++ src/main/kotlin/com/nylas/models/TrackingOptions.kt
@@ -4,8 +4,20 @@ package com.nylas.models
  * Class representing the different tracking options for when a message is sent.
  */
 data class TrackingOptions(
+  /**
+   * The label to apply to tracked messages.
+   */
   val label: String? = null,
-  val links: String? = null,
-  val opens: String? = null,
-  val threadReplies: String? = null,
+  /**
+   * Whether to track links.
+   */
+  val links: Boolean? = null,
+  /**
+   * Whether to track opens.
+   */
+  val opens: Boolean? = null,
+  /**
+   * Whether to track thread replies.
+   */
+  val threadReplies: Boolean? = null,
 )
