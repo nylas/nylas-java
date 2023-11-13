53e989a30b343723fcf8995c8caa366fc3eb5071
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-11-13 16:18:10 -0500

    Order should be optional

--- src/main/kotlin/com/nylas/models/GetAvailabilityResponse.kt
+++ src/main/kotlin/com/nylas/models/GetAvailabilityResponse.kt
@@ -7,14 +7,14 @@ import com.squareup.moshi.Json
  */
 data class GetAvailabilityResponse(
   /**
-   * This property is only populated for round-robin events.
-   * It will contain the order in which the accounts would be next in line to attend the proposed meeting.
-   */
-  @Json(name = "order")
-  val order: List<String>,
-  /**
    * The available time slots where a new meeting can be created for the requested preferences.
    */
   @Json(name = "time_slots")
   val timeSlots: List<TimeSlot>,
+  /**
+   * This property is only populated for round-robin events.
+   * It will contain the order in which the accounts would be next in line to attend the proposed meeting.
+   */
+  @Json(name = "order")
+  val order: List<String>? = null,
 )
