d96db62b4c76e1570f0b91beca72c60909228375
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-11-16 16:20:18 -0500

    AV-2790 Add support for downloading attachment

--- src/main/kotlin/com/nylas/resources/Attachments.kt
+++ src/main/kotlin/com/nylas/resources/Attachments.kt
@@ -2,6 +2,7 @@ package com.nylas.resources
 
 import com.nylas.NylasClient
 import com.nylas.models.*
+import okhttp3.ResponseBody
 
 /**
  * Nylas Attachments API
@@ -22,5 +23,37 @@ class Attachments(client: NylasClient) : Resource<Attachment>(client, Attachment
   fun find(identifier: String, attachmentId: String, queryParams: FindAttachmentQueryParams): Response<Attachment> {
     val path = String.format("v3/grants/%s/attachments/%s", identifier, attachmentId)
     return findResource(path, queryParams)
+  }
+
+  /**
+   * Download the attachment data
+   *
+   * This method returns a [ResponseBody] which can be used to stream the attachment data,
+   * and exposes useful headers such as the content type and content length.
+   *
+   * Alternatively, you can use [downloadBytes] to download the attachment as a byte array.
+   *
+   * @param identifier Grant ID or email account to query
+   * @param attachmentId The id of the attachment to download.
+   * @param queryParams The query parameters to include in the request
+   * @return The [ResponseBody] containing the file data
+   */
+  @Throws(NylasOAuthError::class, NylasSdkTimeoutError::class)
+  fun download(identifier: String, attachmentId: String, queryParams: FindAttachmentQueryParams): ResponseBody {
+    val path = String.format("v3/grants/%s/events/%s/download", identifier, attachmentId)
+
+    return client.downloadResponse(path, queryParams)
+  }
+
+  /**
+   * Download the attachment as a byte array
+   * @param identifier Grant ID or email account to query
+   * @param attachmentId The id of the attachment to download.
+   * @param queryParams The query parameters to include in the request
+   * @return The raw file data
+   */
+  @Throws(NylasOAuthError::class, NylasSdkTimeoutError::class)
+  fun downloadBytes(identifier: String, attachmentId: String, queryParams: FindAttachmentQueryParams): ByteArray {
+    return download(identifier, attachmentId, queryParams).bytes()
   }
 }
