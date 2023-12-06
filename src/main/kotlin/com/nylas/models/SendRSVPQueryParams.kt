559b47e879944c785e118a9d46f93b0de85f2720
YIFAN WU <yifan.w@nylas.com>
2023-12-05 21:22:42 -0500

    Update enum

--- src/main/kotlin/com/nylas/models/SendRSVPQueryParams.kt
+++ src/main/kotlin/com/nylas/models/SendRSVPQueryParams.kt
@@ -3,6 +3,19 @@ package com.nylas.models
 import com.squareup.moshi.Json
 
 /**
+ * Enum representing the allowed RSVP status values.
+ */
+enum class RsvpStatus(val value: String) {
+  YES("yes"),
+  NO("no"),
+  MAYBE("maybe");
+
+  override fun toString(): String {
+    return value
+  }
+}
+
+/**
  * Class representation of the query parameters for sending RSVP.
  */
 data class SendRsvpQueryParams(
@@ -10,14 +23,14 @@ data class SendRsvpQueryParams(
    * The RSVP status for the event. Must be yes, no, or maybe
    */
   @Json(name = "status")
-  val status: String
+  val status: RsvpStatus
 ) : IQueryParams {
 
   /**
    * Builder for [SendRsvpQueryParams].
    * @param status The RSVP status for the event. Must be yes, no, or maybe
    */
-  data class Builder(private val status: String) {
+  data class Builder(private val status: RsvpStatus) {
 
     /**
      * Builds a [SendRsvpQueryParams] instance.
