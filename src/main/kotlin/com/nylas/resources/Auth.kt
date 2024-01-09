77c010a30f6c6a15cf871ee26063a5f9c039c2cb
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2024-01-08 19:04:10 -0500

    AV-2439 Fix scope encoding for OAuth URL

--- src/main/kotlin/com/nylas/resources/Auth.kt
+++ src/main/kotlin/com/nylas/resources/Auth.kt
@@ -165,7 +165,11 @@ class Auth(private val client: NylasClient) {
       .adapter(UrlForAuthenticationConfig::class.java)
       .toJson(config)
     JsonHelper.jsonToMap(json).forEach { (key, value) ->
-      url.addQueryParameter(key, value.toString())
+      if(key == "scope") {
+        url.addQueryParameter("scope", config.scope?.joinToString(separator = " "))
+      } else {
+        url.addQueryParameter(key, value.toString())
+      }
     }
 
     return url
