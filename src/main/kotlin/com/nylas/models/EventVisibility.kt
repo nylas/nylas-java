3b3d13ce32ff33dc04c6abc627873c626269b7d6
Blag <atejada@gmail.com>
2024-01-31 07:12:06 -0500

    Add default as a type of visibillity
    Default is actually a type of visibility

--- src/main/kotlin/com/nylas/models/EventVisibility.kt
+++ src/main/kotlin/com/nylas/models/EventVisibility.kt
@@ -6,6 +6,9 @@ import com.squareup.moshi.Json
  * Enum representation of visibility of the event, if the event is private or public.
  */
 enum class EventVisibility {
+  @Json(name = "default")
+  DEFAULT,
+
   @Json(name = "public")
   PUBLIC,
 
