a8e2e33b91fc84515e9d55be658fc8f0d995b01c
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2024-09-25 12:57:58 -0400

    Add a headers field for Nylas API errs

--- src/main/kotlin/com/nylas/models/AbstractNylasApiError.kt
+++ src/main/kotlin/com/nylas/models/AbstractNylasApiError.kt
@@ -5,9 +5,11 @@ package com.nylas.models
  * @param message The error message.
  * @param statusCode The HTTP status code of the error response.
  * @param requestId The unique identifier of the request.
+ * @param headers The HTTP headers of the error response.
  */
 sealed class AbstractNylasApiError(
   override val message: String,
   open var statusCode: Int? = null,
   open var requestId: String? = null,
+  open var headers: Map<String, List<String>>? = null,
 ) : Exception(message)
